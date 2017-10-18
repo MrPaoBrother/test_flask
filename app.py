e702995b492a62c95dcfccff9402b25915312b81
MrPaoBrother <799906484@qq.com>
2017-10-19 00:13:37 +0800

    chat

--- app.py
+++ app.py
@@ -7,12 +7,15 @@ from flask import request
 
 from flask import render_template
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 app = Flask(__name__)
 
 @app.route('/' , methods=['GET' , 'POST'])
 def main():
-    return render_template('index.html' , title="Power" , message = "逸宁出来聊天吗?")
+    return render_template('index.html' , title="Power" , message = "逸宁 出来聊天吗?")
 
 if __name__ == '__main__':
     app.run(host='45.77.150.109')
