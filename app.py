e2bff630a097fcaec3f2a1494260fa3b6cf96cba
MrPaoBrother <799906484@qq.com>
2017-10-19 00:08:56 +0800

    haha

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/' , methods=['GET' , 'POST'])
 def main():
-    return render_template('index.html' , title="Power" , message = "HelloWorld")
+    return render_template('index.html' , title="Power" , message = "逸宁出来聊天吗?")
 
 if __name__ == '__main__':
     app.run(host='45.77.150.109')
