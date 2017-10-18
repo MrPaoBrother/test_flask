4927e448297c76c5952065057a66145b131ea35d
MrPaoBrother <799906484@qq.com>
2017-10-19 00:44:28 +0800

    like

--- app.py
+++ app.py
@@ -15,8 +15,13 @@ app = Flask(__name__)
 
 @app.route('/' , methods=['GET' , 'POST'])
 def main():
-    return render_template('index.html' , title="Power" , message = "逸宁 出来聊天吗?")
+    return render_template('index.html' , title="Power" , message = "逸宁 你喜欢什么样子的男生?")
+@app.route('/question' , methods=['POST'])
+def answer():
+    answer = request.form["answer"]
+    print "答案是====================>%s"%(answer)
+    return render_template('sleep.html' , title="Sorry啊  哈哈哈" , message = "晚安!")
 
 if __name__ == '__main__':
-    app.run(host='45.77.150.109')
+    app.run(host='127.0.0.1')
     
---
+++ static/js/spider.js
@@ -0,0 +1,11 @@
+function addRules(){
+    var parents = document.getElementById("rules"); //规则
+
+    var btn_add = document.getElementById("add_rules"); //添加按钮
+
+    btn_add.onclick = function(){
+        alert("a");
+    }
+}
+
+addRules();
