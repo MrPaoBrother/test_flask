604fe8bd8d1852e33bfe686cc51b3cdd19ac91dd
MrPaoBrother <799906484@qq.com>
2017-10-18 23:52:42 +0800

    flask test

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+from flask import request
+
+from flask import render_template
+
+
+app = Flask(__name__)
+
+@app.route('/' , methods=['GET' , 'POST'])
+def main():
+    return render_template('index.html' , title="Power" , message = "HelloWorld")
+
+if __name__ == '__main__':
+    app.run()
+    
