e058f535838eb44aebea2ac86182fb721219ff55
MrPaoBrother <799906484@qq.com>
2017-10-19 13:32:35 +0800

    music

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from flask import request
 
 from flask import render_template
 
+import config
+
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -15,7 +17,7 @@ app = Flask(__name__)
 
 @app.route('/' , methods=['GET' , 'POST'])
 def main():
-    return render_template('index.html' , title="Power" , message = "逸宁 你喜欢什么样子的男生?")
+    return render_template('index.html' , title="Hi" , music = "带你去履行")
 @app.route('/question' , methods=['POST'])
 def answer():
     answer = request.form["answer"]
@@ -27,5 +29,5 @@ def answer_get():
     return render_template('sleep.html' , title="Sorry啊  哈哈哈" , message = "晚安!")
 
 if __name__ == '__main__':
-    app.run(host='45.77.150.109')
+    app.run(host=config.REMOTE_IP)
     
---
+++ config.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env Python
+# coding=utf-8
+
+
+REMOTE_IP = '45.77.150.109'
+REMOTE_PASSWORD="d(8Vr!zmmaYrbW?y"
+DEFAULT_IP = '127.0.0.1'
